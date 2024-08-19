4edb670523a513b660b7302f0515df8e9957eae7
Michal Kraninger <kraninger.m@seznam.cz>
2024-08-19 08:33:41 +0200

    Commit Toggler

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import "./App.css"
 import PageContainer from "./components/PageContainer/PageContainer"
+import Toggler from "./components/Toggler/Toggler"
 
 function App() {
-  return <PageContainer></PageContainer>
+  return (
+    <PageContainer>
+      <h1>Your app for hadnling projects</h1>
+      <br></br>
+      <h2>Toggle view</h2>
+      <Toggler />
+    </PageContainer>
+  )
 }
 
 export default App
