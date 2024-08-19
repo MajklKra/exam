0556dc24c50a05c0abbc6ff29f6948b2dab1fbb3
Michal Kraninger <kraninger.m@seznam.cz>
2024-08-19 10:19:12 +0200

    Commit buttons

--- src/App.js
+++ src/App.js
@@ -1,14 +1,86 @@
 import "./App.css"
+import { useState } from "react"
 import PageContainer from "./components/PageContainer/PageContainer"
 import Toggler from "./components/Toggler/Toggler"
+import rawData from "./data.json"
+import ProgrammerList from "./components/ProgrammerList/ProgrammerList"
+import ProgForm from "./components/ProgForm/ProgForm"
 
 function App() {
+  const [listOfProgrammers, setListOfProgrammers] = useState(
+    rawData.programmers
+  )
+
+  const [valid, setValid] = useState(false)
+
+  const [newProgrammer, setNewProgrammer] = useState({
+    id:
+      listOfProgrammers.length > 0
+        ? Math.max(...listOfProgrammers.map((prog) => prog.id)) + 1
+        : 1,
+    name: "",
+    position: "",
+  })
+
+  const handleChange = (e) => {
+    const source = e.target.name
+    const val = e.target.value
+    let updatedProgrammer
+    switch (source) {
+      case "name": {
+        updatedProgrammer = { ...newProgrammer, name: val }
+        break
+      }
+      case "position": {
+        updatedProgrammer = { ...newProgrammer, position: val }
+        break
+      }
+
+      default:
+        break
+    }
+    setNewProgrammer(updatedProgrammer)
+  }
+
+  const validateData = (prog) => {
+    if (prog.name.trim().length === 0 || prog.position.trim().length === 0) {
+      setValid(false)
+    } else {
+      setValid(true)
+    }
+  }
+
+  const handleAdd = () => {
+    setListOfProgrammers((listOfProgrammers) => {
+      return [...listOfProgrammers, newProgrammer]
+    })
+    const updatedProgrammer = {
+      id: newProgrammer.id + 1,
+      name: "",
+      position: "",
+    }
+    setNewProgrammer(updatedProgrammer)
+    validateData(updatedProgrammer)
+  }
+
+  const handleDelete = (idToDel) => {
+    const temp = listOfProgrammers.filter((prog) => prog.id !== idToDel)
+    setListOfProgrammers(temp)
+  }
+
   return (
     <PageContainer>
-      <h1>Your app for hadnling projects</h1>
+      <h1>Your app for handling projects</h1>
       <br></br>
       <h2>Toggle view</h2>
       <Toggler />
+      <ProgrammerList data={listOfProgrammers} onDelete={handleDelete} />
+      <ProgForm
+        valid={valid}
+        onChange={handleChange}
+        onAdd={handleAdd}
+        data={newProgrammer}
+      />
     </PageContainer>
   )
 }
---
+++ src/components/ProgForm/ProgForm.jsx
@@ -0,0 +1,41 @@
+import "./ProgForm.css"
+
+function ProgForm({ data, valid, onChange, onAdd }) {
+  return (
+    <div className="prog-form">
+      <input
+        type="text"
+        placeholder="jmeno programatora"
+        name="name"
+        onChange={onChange}
+        value={data.name}
+      />
+
+      <label htmlfor="junior">Junior</label>
+      <input
+        id="junior"
+        type="radio"
+        name="position"
+        onChange={onChange}
+        value="Junior"
+        checked={data.position === "Junior"}
+      />
+
+      <label htmlfor="senior">Senior</label>
+      <input
+        id="senior"
+        type="radio"
+        name="position"
+        onChange={onChange}
+        value="Senior"
+        checked={data.position === "Senior"}
+      />
+
+      <button disabled={valid} onClick={onAdd}>
+        Přidat
+      </button>
+    </div>
+  )
+}
+
+export default ProgForm
